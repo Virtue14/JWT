a3cd8c4ef93b254971f8cbddc15b8ffbbef9366a
악덕 <rudejrqkr97@gmail.com>
2024-05-22 01:12:02 +0900

    feat: 로그인 필터 구현
    - SecurityConfig에 AuthenticationManager Bean을 추가하였습니다.
    - 이제 LoginFilter에서 AuthenticationManager를 사용하여 로그인을 처리할 수 있습니다.
    - 코드의 보안성을 향상시켰습니다.

--- src/main/java/com/virtue/securityjwt/config/SecurityConfig.java
+++ src/main/java/com/virtue/securityjwt/config/SecurityConfig.java
@@ -1,18 +1,37 @@
 package com.virtue.securityjwt.config;
 
+import com.virtue.securityjwt.jwt.LoginFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -34,7 +53,11 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
+
+        // Session 설정
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
