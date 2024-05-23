43cb151edc2b39a9b3ab8596eb142eceb15967fe
악덕 <rudejrqkr97@gmail.com>
2024-05-23 09:30:50 +0900

    feat: DB기반 로그인 검증 로직
    - 사용자 인증 정보를 표시하기 위한 CustomUserDetails 클래스 추가
    - 사용자의 권한을 표시하기 위해 getAuthorities 메소드 구현
    - getPassword, getUsername 메소드를 구현하여 사용자 정보 반환
    - isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired, isEnabled 메소드 구현
    - UserEntity 객체를 UserDetails로 변환하여 반환
    - UserRepository 인터페이스에 findByUsername 메소드 추가
    - CustomUserDetailsService 클래스 추가 및 loadUserByUsername 메소드 구현
    - LoginFilter 클래스에 성공, 실패 인증 처리 추가

--- src/main/java/com/virtue/securityjwt/repository/UserRepository.java
+++ src/main/java/com/virtue/securityjwt/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Boolean existsByUsername(String username);
+
+    // 회원을 조회하는 메소드 작성
+    UserEntity findByUsername(String username);
 }
---
+++ src/main/java/com/virtue/securityjwt/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.virtue.securityjwt.service;
+
+import com.virtue.securityjwt.dto.CustomUserDetails;
+import com.virtue.securityjwt.entity.UserEntity;
+import com.virtue.securityjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
