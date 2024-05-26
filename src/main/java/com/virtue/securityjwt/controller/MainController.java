17652d84d0c374e78d323bdd00b56ecc3c4ddec0
악덕 <rudejrqkr97@gmail.com>
2024-05-27 06:10:42 +0900

    feat: 세션 사용자 정보 표시
    - 세션 현재 사용자 아이디를 표시합니다.
    - 세션 현재 사용자 role을 표시합니다.

--- src/main/java/com/virtue/securityjwt/controller/MainController.java
+++ src/main/java/com/virtue/securityjwt/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.virtue.securityjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -10,6 +16,16 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "Main Controller";
+
+        // 세션 현재 사용자 아이디
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 세션 현재 사용자 role
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        return "Main Controller " + username + " "+ role;
     }
 }
